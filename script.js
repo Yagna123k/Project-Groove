9c62dac7f21c723da91da0d0710099d388073711
Yagna123k <yagna82kb@gmail.com>
2023-08-27 16:42:50 +0530

    Commit by Yagna

---
+++ script.js
@@ -0,0 +1,9 @@
+var buy = document.getElementById("Buy")
+
+buy.onclick=()=>{
+    buy.style.backgroundColor = "transparent"
+    buy.style.width = "400px"
+    buy.style.color = "#000000"
+    buy.innerHTML = "Thank You For Shopping With Us."  
+    buy.style.fontSize="20px"
+}
